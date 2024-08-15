90ab814f51add8f162e6dc6fab742bafb0ea0aa4
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-15 17:12:18 +0800

    Initial Commmit

---
+++ View/LoginView.xaml.vb
@@ -0,0 +1,9 @@
+﻿Public Class LoginView
+    Private Sub Window_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        If e.LeftButton = MouseButtonState.Pressed Then
+
+            DragMove()
+
+        End If
+    End Sub
+End Class
