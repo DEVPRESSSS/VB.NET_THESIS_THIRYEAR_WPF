4e1ccd90a8d3a75f3d2c3e00ef4edcf36e78caeb
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-15 21:54:08 +0800

    Login Top Panel and Widgets changes

--- View/LoginView.xaml.vb
+++ View/LoginView.xaml.vb
@@ -6,4 +6,17 @@
 
         End If
     End Sub
+
+    Private Sub btnMinimize_Click(sender As Object, e As RoutedEventArgs)
+        WindowState = WindowState.Minimized
+    End Sub
+
+    Private Sub btnMaximize_Click(sender As Object, e As RoutedEventArgs)
+        WindowState = WindowState.Maximized
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As RoutedEventArgs)
+        Application.Current.Shutdown()
+    End Sub
 End Class
